1232e0d31b3ba728cf7b6fa52629895ed186b796
Jimmy62138 <jimmy.warnault@outlook.com>
2019-08-13 19:52:30 +0200

    good
    good

--- posts/src/app/app.module.ts
+++ posts/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostListComponent } from './post-list/post-list.component';
-import { PostListItemComponent } from './post-list-item/post-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PostListComponent,
-    PostListItemComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
